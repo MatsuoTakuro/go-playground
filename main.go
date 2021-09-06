fc1e9a6921aaa4ee9e243e3e7e75864812684fde
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-09-06 19:05:36 +0900

    新ゴールーチン起動時にiの値を引数として渡す

--- main.go
+++ main.go
@@ -1,51 +1,28 @@
 package main
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
-	"io"
-	"log"
-	"strings"
-	"unicode/utf8"
+	"time"
 )
 
-type RuneScanner struct {
-	r   io.Reader
-	buf [16]byte
-}
-
-func NewRuneScanner(r io.Reader) *RuneScanner {
-	return &RuneScanner{r: r}
-}
-
-func (s *RuneScanner) Scan() (rune, error) {
-	n, err := s.r.Read(s.buf[:])
-	if err != nil {
-		return 0, err
-	}
-
-	r, size := utf8.DecodeRune(s.buf[:n])
-	if r == utf8.RuneError {
-		return 0, errors.New("RuneError")
-	}
-
-	s.r = io.MultiReader(bytes.NewReader(s.buf[size:n]), s.r)
-	return r, nil
-}
-
 func main() {
-	s := NewRuneScanner(strings.NewReader("Hello, 世界"))
-	for {
-		r, err := s.Scan()
-		if err == io.EOF {
-			break
-		}
 
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%c\n", r)
+	for i := 0; i < 3; i++ {
+		/*
+		   go func() {
+		       fmt.Println(i)
+		   }()
+		*/
+		go func(i int) {
+			fmt.Println(i)
+		}(i)
+		time.Sleep(100 * time.Millisecond)
 	}
+	/*
+	   (実行結果)
+	   0
+	   2
+	   1
+	   (0,1,2が順不同で出力)
+	*/
 }
